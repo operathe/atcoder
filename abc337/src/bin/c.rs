4c66a32bbb2d90343e0b5e363a1810143a43aea0
operathe <operathe11@gmail.com>
2024-04-18 18:17:33 +0900

    abc337 c

--- abc337/src/bin/c.rs
+++ abc337/src/bin/c.rs
@@ -4,8 +4,7 @@ use itertools::{iproduct, Itertools};
 use num_traits::pow;
 #[allow(unused_imports)]
 use proconio::{
-    fastout,
-    input,
+    fastout, input,
     marker::{Chars, Usize1},
 };
 #[allow(unused_imports)]
@@ -15,12 +14,24 @@ use std::collections::{HashMap, HashSet, VecDeque};
 #[allow(unused_imports)]
 use std::iter::FromIterator;
 #[allow(non_snake_case)]
-
 #[fastout]
+
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input! {
+        n: usize,
+        a: [i32; n],
     }
+    let mut next = vec![n; n];
+    for (i, a) in a.iter().enumerate() {
+        if *a != -1 {
+            next[*a as usize - 1] = i;
+        }
+    }
+    let mut ans = vec![];
+    let mut pos = a.iter().position(|a| *a == -1).unwrap();
+    while pos != n {
+        ans.push(pos + 1);
+        pos = next[pos];
+    }
+    println!("{}", ans.iter().join(" "));
 }
