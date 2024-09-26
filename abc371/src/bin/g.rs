12156f724177c37ac14e1c286d3175bebc9e949b
operathe <operathe11@gmail.com>
2024-09-26 20:21:36 +0900

    abc372.3

--- abc371/src/bin/g.rs
+++ abc371/src/bin/g.rs
@@ -1,21 +1,21 @@
 #![allow(unused_imports, unused_variables, dead_code, non_snake_case)]
 use ac_library::*;
 use itertools::*;
-use num_traits::{pow, abs};
+use num_traits::{abs, pow};
 use proconio::{fastout, input, marker::*};
 use std::cmp::{max, min};
-use superslice::*;
 use std::collections::*;
+use superslice::*;
 
 type Mint = ModInt998244353;
 
 #[fastout]
 fn main() {
-    input!{
+    input! {
         h: String,
         n: usize,
         w: Usize1,
-        s: [Chars; h],
-        mut plan: [(usize, usize); h]
+        s: [Chars; n],
+        mut plan: [(usize, usize); n]
     }
 }
