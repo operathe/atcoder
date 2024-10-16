30b5f38258ddb58d2dfd3c7e472451a79c1b6eaf
operathe <operathe11@gmail.com>
2024-10-16 23:16:02 +0900

    abc375.2

--- abc375/src/bin/a.rs
+++ abc375/src/bin/a.rs
@@ -1,21 +1,30 @@
 #![allow(unused_imports, unused_variables, dead_code, non_snake_case)]
 use ac_library::*;
 use itertools::*;
-use num_traits::{pow, abs};
+use num_traits::{abs, pow};
 use proconio::{fastout, input, marker::*};
 use std::cmp::{max, min};
-use superslice::*;
 use std::collections::*;
+use superslice::*;
 
 type Mint = ModInt998244353;
 
 #[fastout]
 fn main() {
-    input!{
-        h: String,
+    input! {
         n: usize,
-        w: Usize1,
-        s: [Chars; n],
-        mut plan: [(usize, usize); n]
+        s: Chars,
     }
+
+    let mut ans = 0;
+    if n <= 2 {
+        println!("{}", ans);
+        return;
+    }
+    for i in 0..(n - 2) {
+        if s[i] == '#' && s[i + 1] == '.' && s[i + 2] == '#' {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
--- abc375/src/bin/b.rs
+++ abc375/src/bin/b.rs
@@ -1,21 +1,29 @@
 #![allow(unused_imports, unused_variables, dead_code, non_snake_case)]
 use ac_library::*;
 use itertools::*;
-use num_traits::{pow, abs};
+use libm::sqrt;
+use num_traits::{abs, pow};
 use proconio::{fastout, input, marker::*};
 use std::cmp::{max, min};
-use superslice::*;
 use std::collections::*;
+use superslice::*;
 
 type Mint = ModInt998244353;
 
 #[fastout]
 fn main() {
-    input!{
-        h: String,
+    input! {
         n: usize,
-        w: Usize1,
-        s: [Chars; n],
-        mut plan: [(usize, usize); n]
+        mut point: [(i64, i64); n]
     }
+    point.push((0, 0));
+    let mut prev_x = 0;
+    let mut prev_y = 0;
+    let mut ans = 0.0;
+    for (x, y) in point {
+        ans += (((prev_x - x).pow(2) + (prev_y - y).pow(2)) as f64).sqrt();
+        prev_x = x;
+        prev_y = y;
+    }
+    println!("{}", ans);
 }
