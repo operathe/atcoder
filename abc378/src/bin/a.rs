5d4788293e42b33dcf78e670f01f4e1a7851e146
operathe <operathe11@gmail.com>
2024-11-04 20:44:57 +0900

    abc378

---
+++ abc378/src/bin/a.rs
@@ -0,0 +1,21 @@
+#![allow(unused_imports, unused_variables, dead_code, non_snake_case)]
+use ac_library::*;
+use itertools::*;
+use num_traits::{abs, pow};
+use proconio::{fastout, input, marker::*};
+use std::cmp::{max, min};
+use std::collections::*;
+use superslice::*;
+
+type Mint = ModInt998244353;
+
+#[fastout]
+fn main() {
+    input! {
+        h: String,
+        n: usize,
+        w: Usize1,
+        s: [Chars; n],
+        mut plan: [(usize, usize); n]
+    }
+}
