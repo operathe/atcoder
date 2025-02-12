e30bda1886e0a395618d44a2444977b89ff8ef2a
operathe <operathe11@gmail.com>
2025-02-12 22:09:05 +0900

    abc378.2

--- abc366/src/bin/a.rs
+++ abc366/src/bin/a.rs
@@ -12,20 +12,22 @@ use proconio::{fastout, input, marker::*};
 use std::cmp::{max, min};
 use std::collections::*;
 use superslice::*;
-
 type Mint = ModInt998244353;
 
 #[fastout]
+
 fn main() {
     input! {
         n: usize,
         t: usize,
         a: usize,
     }
+
     let result = match (t > (n / 2), a > (n / 2)) {
         (true, _) => "Yes",
         (_, true) => "Yes",
         _ => "No",
     };
+
     println!("{}", result);
 }
